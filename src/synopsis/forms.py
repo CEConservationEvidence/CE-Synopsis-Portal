d308b978bc54c997517223cc5ee45f5a64905113
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2025-09-11 20:57:26 +0100

    Add ProtocolUpdateForm and CreateUserForm
    Will add assign role form as TODO.

--- src/synopsis/forms.py
+++ src/synopsis/forms.py
@@ -1,1 +1,17 @@
 from django import forms
+from .models import Protocol
+
+
+class ProtocolUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Protocol
+        fields = ["document", "text_version"]
+        widgets = {"text_version": forms.Textarea(attrs={"rows": 6})}
+
+
+class CreateUserForm(forms.Form):
+    first_name = forms.CharField(max_length=150)
+    last_name = forms.CharField(max_length=150, required=False)
+    password = forms.CharField(
+        max_length=128, required=False, widget=forms.PasswordInput
+    )
