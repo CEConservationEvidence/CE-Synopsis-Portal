c125b11c29b1effbbc2513ea908316f5db9202a4
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2025-09-11 19:21:15 +0100

    added missing import uuid

--- src/synopsis/models.py
+++ src/synopsis/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 """
 TODO: Modularise models into synopsis/models/* (project.py, funding.py, protocol.py, etc.)
