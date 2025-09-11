615686629e5b7066a5ea8291ea2bf2ccd8fc0f02
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2025-09-11 21:14:25 +0100

    add draft assignrole form

--- src/synopsis/forms.py
+++ src/synopsis/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Protocol
+from django.contrib.auth.models import User
+from .models import Protocol, UserRole
 
 
 class ProtocolUpdateForm(forms.ModelForm):
@@ -12,6 +13,12 @@ class ProtocolUpdateForm(forms.ModelForm):
 class CreateUserForm(forms.Form):
     first_name = forms.CharField(max_length=150)
     last_name = forms.CharField(max_length=150, required=False)
+    email = forms.EmailField(help_text="Used as the username")
     password = forms.CharField(
         max_length=128, required=False, widget=forms.PasswordInput
     )
+
+
+class AssignRoleForm(forms.Form):
+    user = forms.ModelChoiceField(queryset=User.objects.order_by("username"))
+    role = forms.ChoiceField(choices=UserRole.ROLE_CHOICES)
