7c31be555ed39c79bb9d4ea0de467c3876f2976f
Ibrahim Alhas <65875290+alhasacademy96@users.noreply.github.com>
2025-10-01 09:56:21 +0100

    added title field to AB member datamodel

--- src/synopsis/models.py
+++ src/synopsis/models.py
@@ -408,6 +408,7 @@ class AdvisoryBoardMember(models.Model):
     )
 
     # Basic information on the member
+    title = models.CharField(max_length=20, blank=True)
     first_name = models.CharField(max_length=100)
     middle_name = models.CharField(max_length=100, blank=True)
     last_name = models.CharField(max_length=100, blank=True)
@@ -456,7 +457,9 @@ class AdvisoryBoardMember(models.Model):
     participation_statement = models.TextField(blank=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name or ''} ({self.email})"
+        parts = [self.title, self.first_name, self.last_name or ""]
+        name = " ".join(part for part in parts if part).strip()
+        return f"{name} ({self.email})"
 
     @property
     def latest_protocol_feedback(self):
